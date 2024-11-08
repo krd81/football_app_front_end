c17367b95ccd708f4efb4536fb153665cbc6769f
Kelly <kelly.degannes@gmail.com>
2024-11-08 22:45:15 +1100

    Added fonts and background image to homepage

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -5,9 +5,10 @@ const Homepage = () => {
     <>
     <div className='homepage'>
 
-        <div><h1>Football Predictions</h1></div>
-        <div><h1>2024/2025 Season</h1></div>
-        <div><h2>Current game week: </h2></div>
+        <div><h1 className='paytone-one-h1'>Football Predictions</h1></div>
+        <div><h1 className='paytone-one-h1'>2024/2025 Season</h1></div>
+        <div><h2 className='paytone-one-h2'>Current game week</h2></div>
+        <div><h2> </h2></div>
     </div>
     </>
   )
