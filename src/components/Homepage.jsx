26d7730303a36d6b256229f1932dba86ee706bd7
Kelly <kelly.degannes@gmail.com>
2024-11-08 21:04:14 +1100

    Homepage layout changes

---
+++ src/components/Homepage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Homepage = () => {
+  return (
+    <>
+    <div className='homepage'>
+
+        <div><h1>Football Predictions</h1></div>
+        <div><h1>2024/2025 Season</h1></div>
+        <div><h2>Current game week: </h2></div>
+    </div>
+    </>
+  )
+}
+
+export default Homepage
