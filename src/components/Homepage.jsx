ee87ba72ae88dc5315b68a48b62f0bf0ddeda030
Kelly <kelly.degannes@gmail.com>
2024-11-12 23:02:31 +1100

    Added title animation effect

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import '../css/title-animation.css'
 
 const Homepage = () => {
   return (
     <>
     <div className='homepage'>
 
-        <div><h1 className='train-one-regular-h1'>Football Predictions</h1></div>
+        <div><h1 className='title-hero'>Football Predictions</h1></div>
         <div><h1 className='paytone-one-h1'>2024/2025 Season</h1></div>
         <div><h2 className='paytone-one-h2'>Current game week</h2></div>
         <div><h2> </h2></div>
