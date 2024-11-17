1edadc2145268a3e0bc163313f45d60a26d372bf
Kelly <kelly.degannes@gmail.com>
2024-11-17 21:48:56 +1100

    Extracted Title component for re-usability

---
+++ src/common/FlashTitle.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../css/title-animation.css'
+
+const FlashTitle = () => {
+  return (
+    <>
+        <h1 className='title-hero'>Football Predictions</h1>
+    </>
+  )
+}
+
+export default FlashTitle
