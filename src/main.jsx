a6fd5fc4fbe8ca4a5db58a5b63bc1e0edd132d3b
Kelly <kelly.degannes@gmail.com>
2024-11-27 19:46:17 +1100

    Moved context.provider to App

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './css/index.css'
 import App from './components/App.jsx'
-import { AppContextProvider } from './authentication/AppContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <AppContextProvider>
       <App />
-    </AppContextProvider>
   </StrictMode>
 )
