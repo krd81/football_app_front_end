4513db425db08cf0ed717538a8c81c7bab7f0817
Kelly <kelly.degannes@gmail.com>
2024-12-05 22:06:16 +1100

    Replaced useEffect with useMemo in Predictions component

--- src/components/Predictions.jsx
+++ src/components/Predictions.jsx
@@ -1,23 +1,16 @@
-import { Fragment, useState, useEffect, useContext } from 'react'
+import { Fragment, useContext, useMemo } from 'react'
 import '../css/Scores.css'
 import { AppContext } from '../authentication/AppContext';
-import { CompRoundContext } from '../common/CompRoundContext';
+
 
 const Predictions = ({ round }) => {
   const { fixtures } = useContext(AppContext);
-  // const {selectedRound } = useContext(CompRoundContext);
-  const [currentFixtures, setCurrentFixtures] = useState([]);
 
-  // for (let fixture in fixtures) {
-  //   for (let matchElement in fixtures[fixture]) {
-  //       if (matchElement === 'round' &&
-  //           fixtures[fixture][matchElement] === round) {
-  //           setCurrentFixtures([...currentFixtures, fixtures[fixture]]);
-  //       }
-  //   }
-  // }
+  const currentFixtures = useMemo(() => {
+    return getRoundFixtures(fixtures, round);
+  }, [fixtures, round]);
 
-  useEffect(() => {
+  function getRoundFixtures (fixtures, round) {
     const newFixtures = [];
     for (let fixture in fixtures) {
         for (let matchElement in fixtures[fixture]) {
@@ -27,52 +20,47 @@ const Predictions = ({ round }) => {
             }
         }
     }
-    setCurrentFixtures(newFixtures);
-  }, [fixtures, round]);
-
+    return newFixtures;
+  }
 
 
   return (
     <>
-    <div>
-      <h1>Predictions:</h1>
-    </div>
-    <div className='match-list'>
-      <form action="prediction-form">
+      <div>
+        <h1>Predictions:</h1>
+      </div>
+      <div className='match-list'>
+        <form action="prediction-form">
+          {currentFixtures?.map((match) => {
+              return (
+                <>
+                  <Fragment key={match._id}>
+                    <div className='match-card' >
+                      <div className='predictions-text'>
+                        <div className='grid-container'>
+                          <div className='grid-item1'>
+                            <label htmlFor='score-predictions'>{`${match.homeName} `}</label>
+                          </div>
+                          <div className='grid-item2'>
+                            <input className='score-input' id={`${match.homeName}`} name='score-predictions' type='text' size="1"></input>
+                            &nbsp;-&nbsp;
+                            <input className='score-input' id={`${match.awayName}`} name='score-predictions' type='text' size="1"></input>
+                          </div>
+                          <div className='grid-item1'>
+                            <label htmlFor='score-predictions'>{` ${match.awayName}`}</label>
+                          </div>
+                        </div>
+                      </div>
+                        <p className='predictions-text'>{`Kick-off: ${match.time}`}</p>
+                        <p className='predictions-text'>{`${match.location}`}</p>
+                    </div>
 
-     {currentFixtures?.map((match) => {
-        return (
-          <>
-            <Fragment key={match._id}>
-          <div className='match-card' >
-            <div className='predictions-text'>
-              <div className='grid-container'>
-              <div className='grid-item1'>
-                <label htmlFor='score-predictions'>{`${match.homeName} `}</label>
-
-              </div>
-              <div className='grid-item2'>
-              <input className='score-input' id={`${match.homeName}`} name='score-predictions' type='text' size="1"></input>
-            &nbsp;-&nbsp;
-            <input className='score-input' id={`${match.awayName}`} name='score-predictions' type='text' size="1"></input>
-              </div>
-              <div className='grid-item1'>
-
-              <label htmlFor='score-predictions'>{` ${match.awayName}`}</label>
-            </div>
-            </div>
-
-            </div>
-              <p className='predictions-text'>{`Kick-off: ${match.time}`}</p>
-              <p className='predictions-text'>{`${match.location}`}</p>
-            </div>
-
-            </Fragment>
-          </>
-        )
-      })
-    }
-      </form>
+                  </Fragment>
+                </>
+              )
+            })
+          }
+        </form>
       </div>
     </>
   )
