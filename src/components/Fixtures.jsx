787ab2b49bc9aaddf1d0cd46259106163b33dd58
Kelly <kelly.degannes@gmail.com>
2024-12-07 10:53:34 +1100

    Indentation change

--- src/components/Fixtures.jsx
+++ src/components/Fixtures.jsx
@@ -17,11 +17,11 @@ const Fixtures = () => {
      {fixtures?.map((match, index) => {
         return (
           <Fragment key={match._id}>
-          <div className='match-card'>
-            <p>{`${index+1}. ${shortName(match.homeName)} vs ${shortName(match.awayName)}`}</p>
-            <p>{`Date: ${DateFormatter1(match.date)}`}</p>
-            <p>{`Kick-off: ${match.time}`}</p>
-            <p>{`${match.location}`}</p>
+            <div className='match-card'>
+              <p>{`${index+1}. ${shortName(match.homeName)} vs ${shortName(match.awayName)}`}</p>
+              <p>{`Date: ${DateFormatter1(match.date)}`}</p>
+              <p>{`Kick-off: ${match.time}`}</p>
+              <p>{`${match.location}`}</p>
             </div>
           </Fragment>
         )
