f07473e4a49eb05acee3fe3ce7a6af2a6b5f435d
Kelly <kelly.degannes@gmail.com>
2024-12-06 14:41:23 +1100

    Refactored useEffect to useMemo in Play component

--- src/components/Play.jsx
+++ src/components/Play.jsx
@@ -1,5 +1,5 @@
 import '../css/app.css'
-import { useContext, useState, useEffect, useMemo } from "react";
+import { useContext, useMemo } from "react";
 import { AppContext } from '../authentication/AppContext'
 import { CompRoundContext } from '../common/CompRoundContext';
 import GameWeekSelect from './GameWeekSelect';
@@ -8,10 +8,7 @@ import GameWeekSelect from './GameWeekSelect';
 function Play ({ setCompRound }) {
     const { fixtures, selectedCompetition } = useContext(AppContext);
 
-    const [rounds, setRounds] = useState([]);
-
-
-    useEffect(() => {
+    function getRounds (fixtures) {
         const newRounds = [];
         for (let fixture in fixtures) {
             for (let matchElement in fixtures[fixture]) {
@@ -20,39 +17,21 @@ function Play ({ setCompRound }) {
                 }
             }
         }
-        setRounds(newRounds);
-    }, [fixtures]);
-
- /*
-    function getRounds (fixtures) {
-        const newRounds = [];
-        for (let fixture in fixtures) {
-            for (let matchElement in fixtures[fixture]) {
-                if (matchElement === 'round' && !newRounds.includes(fixtures[fixture][matchElement])) {
-                    return newRounds.push(fixtures[fixture][matchElement]);
-                }
-            }
-        }
+        // Sort round numbers numerically
+        return newRounds.sort((a, b) => a - b);
     }
 
     const rounds = useMemo(() => {
         return getRounds(fixtures);
     }, [fixtures]);
-*/
-    // Sort round numbers numerically
-    // const sortedArray = rounds.sort();
-    // sortedRounds.current = sortedArray;
 
-    const sortedRounds = useMemo(() => {
-        return rounds.sort((a, b) => a - b);
-    }, [rounds]);
 
 
 
     return (
         <>
             <CompRoundContext.Provider value={{
-                rounds: sortedRounds
+                rounds
                 }}
             >
                 <GameWeekDisplay comp={selectedCompetition.name}/>
