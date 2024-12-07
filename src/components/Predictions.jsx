2932620f1c2c2e763c5c0b05320f5e553954ec65
Kelly <kelly.degannes@gmail.com>
2024-12-07 15:25:45 +1100

    Added Matchweek number to Predictions component

--- src/components/Predictions.jsx
+++ src/components/Predictions.jsx
@@ -34,7 +34,7 @@ const Predictions = ({ round }) => {
   return (
     <>
       <div>
-        <h1>Predictions:</h1>
+        <h1>Predictions  - Matchweek {round}:</h1>
       </div>
       <div className='match-list'>
         {fixtureDates?.map((fixtureDate) => {
