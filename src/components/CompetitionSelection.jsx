3e51643d93f11188c752bf140c7bc12cd67c236d
Kelly <kelly.degannes@gmail.com>
2024-12-29 23:43:03 +1100

    Added UseCallback event handler

--- src/components/CompetitionSelection.jsx
+++ src/components/CompetitionSelection.jsx
@@ -1,10 +1,12 @@
-import { useContext } from 'react';
+import { useContext, useCallback } from 'react';
 import { AppContext } from '../contexts/AppContext';
+
 
 // Manages the selection of competition and sets this value in state
 // Parent component (Homepage) manages the selectedCompetition state
 const CompetitionSelection = () => {
-  const { competitions, comps, setComp, selectedCompetition, setSelectedCompetition } = useContext(AppContext);
+  const { competitions, selectedCompetition, setSelectedCompetition } = useContext(AppContext);
+  
 
   // If selectedCompetition is null:
   // Set as the first element of competitions[0]
@@ -13,6 +15,19 @@ const CompetitionSelection = () => {
   //   selectedCompetition(competitions[0]);
   // }
 
+  const handleCompSelection = useCallback(e => {
+    let compName = e.target.value;
+    let selectedComp;
+    competitions.map((comp) =>
+      comp.name === compName ?
+        selectedComp = comp : null
+    )
+
+    setSelectedCompetition(selectedComp)
+    console.log(selectedCompetition)
+
+  }, [competitions, selectedCompetition, setSelectedCompetition]);
+
   return (
     <>
     <div className='compDropDown'>
@@ -20,17 +35,7 @@ const CompetitionSelection = () => {
         <select
           name='competition'
           id='competition'
-          onChange={e => {
-            let compName = e.target.value;
-            let selectedComp;
-            competitions.map((comp) =>
-              comp.name === compName ?
-                selectedComp = comp : null
-            )
-            console.log(selectedComp);
-            setSelectedCompetition(selectedComp)
-            console.log(JSON.stringify(selectedCompetition))
-          }}
+          onChange={handleCompSelection}
         >
             {competitions.map((comp, i) =>
                 comp.active?
